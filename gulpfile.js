ee0db931c1bedb887de2dad48eaa1836957c2277
Renny <renny@bunnyhug.net>
2017-04-18 22:44:51 -0600

    7.2 Automate with gulp

---
+++ gulpfile.js
@@ -0,0 +1,11 @@
+var gulp = require('gulp');
+var browserify = require('browserify');
+var source = require('vinyl-source-stream');
+
+gulp.task('default', function(){
+    return browserify('src/App.js')
+        .transform('babelify', {presets: 'react'})
+        .bundle()
+        .pipe(source('bundle.js'))
+        .pipe(gulp.dest('static/'));
+});
