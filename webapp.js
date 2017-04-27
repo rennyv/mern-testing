aa917ffabd9701a64a4b29d0e313988e8599017a
Renny <renny@bunnyhug.net>
2017-04-26 20:31:16 -0600

    10.2 PUT API: update a record

--- webapp.js
+++ webapp.js
@@ -43,6 +43,18 @@ app.get('/api/bugs/:id', function(req, res) {
     });
 });
 
+/* Modify one record, given its Id */
+app.put('/api/bugs/:id', function(req,res){
+    var bug = req.body;
+    console.log("Modifying bugs:", req.params.id, bug);
+    var oid = ObjectId(req.params.id);
+    db.collection("bugs").updateOne({_id: oid}, bug, function(err, result){
+        db.collection("bugs").find({_id: oid}).next(function(err, doc){
+            res.send(doc);
+        });
+    });
+});
+
 app.get('*', function(req, res){
     res.sendFile(__dirname + '/static/index.html');
 })
